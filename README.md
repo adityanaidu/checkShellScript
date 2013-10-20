9d8035edcd73af8f6b9b9d06e81d5770f7e03f4e
Aditya Naidu <aditya.naidu@gmail.com>
2013-10-20 10:53:41 -0400

    updated readme. code refactoring. fixed a bug. added checks

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ checkShellScript
 ================
 
 Static Code Analysis of bash scripts
+
+Developed on Python 2.7.3 
+
+For Usage type
+  ./checkShellScript.py -h
+
+Unit Testing
+To test the code itself run checkShellScript on test.lines
+and compare the output with the expected.results file:
+  ./checkShellScript.py test.lines > actual.results
+  diff actual.results expected.results
+
+diff should not produce any output
--- checkShellScript.py
+++ checkShellScript.py
@@ -10,14 +10,18 @@ import os.path
 ## Add elements to checkList2 for additional checks
 checkList2 = []
 
-reexpDA = re.compile("\s*$@\s*")
-checkList2.append((reexpDA, " Need to double quote $@\n"))
+reexpDA1 = re.compile("(\s+\$@|\$@\s+)")
+checkList2.append((reexpDA1, " Need to double quote $@\n"))
+
+reexpDA2 = re.compile("(\s+\$\*|\$\*\s+)")
+checkList2.append((reexpDA2, " Need to double quote $*\n"))
 
 reexpRD = re.compile("2>&1\s+>")    
-checkList2.append((reexpRD, " Need to have > before 2>&1 like \"> logFile 2>&1\"\n"))
+checkList2.append(
+     (reexpRD, " > should be before 2>&1 like \"> logFile 2>&1\"\n"))
 
 reexpAD = re.compile("\$[a-zA-Z0-9]+=") 
-checkList2.append((reexpAD, " $var being assigned a value. Remove $\n"))
+checkList2.append((reexpAD, " $<var> being assigned a value. Remove $\n"))
 
 reexpDD = re.compile("\s+\[.*&&.*\]")	
 checkList2.append((reexpDD, " && in [ ] not allowed\n"))
@@ -29,13 +33,13 @@ reexpTQ = re.compile("\".*~.*\"")
 checkList2.append((reexpTQ, " Remove double quotes around Tilde\n"))
 
 reexpDo = re.compile("[a-zA-Z0-9\}]+\s+do[$\s]")
-checkList2.append((reexpDo, " Missing a ; before do?\n")) 
+checkList2.append((reexpDo, " Missing a ; before do\n")) 
 
 reexpDone = re.compile("[a-zA-Z0-9\}]+\s+done")
 checkList2.append((reexpDone, " Missing a ; before done\n"))
 
 reexpVA = re.compile("(\s+=[^=]|=[^=]\s+)")
-checkList2.append((reexpVA, "Space in variable assignments\n"))
+checkList2.append((reexpVA, " Space in variable assignments\n"))
 
 def usage():
     print "Usage: %s <space separated script files list>" % sys.argv[0]
@@ -50,23 +54,27 @@ def parseFiles(files):
             print "File %s doesn't exists" % fl
             continue
      
-        lNum = 0
         with open(fl, "r") as f:
             content = f.readlines()
         
         if 'bash' not in content[0] \
              or content[0].startswith('#!') == False:
             print "%s is missing a proper #! line\n" % fl   
-     
-        for ln in content:
-            lNum+=1
-            subLn = ln.split('|')  
-            for aTuple in checkList2:
-                for asubLn in subLn:
-                    if aTuple[0].search(asubLn):
-                        print fl, lNum, asubLn,
-                        print aTuple[1]
 
+        checkFile(content, fl)
+
+def checkFile(content, fileName):
+
+    lNum = 0
+
+    for ln in content:
+        lNum+=1
+        subLns = ln.split('|') #treat lines with pipes as multiple lines
+        for aTuple in checkList2:
+            for asubLn in subLns:
+                if aTuple[0].search(asubLn):
+                    print "%s:%d %s" % (fileName, lNum, asubLn),  
+                    print aTuple[1]
  
 if __name__ == "__main__":
     if len(sys.argv) == 1 or sys.argv[1] == "-h":
